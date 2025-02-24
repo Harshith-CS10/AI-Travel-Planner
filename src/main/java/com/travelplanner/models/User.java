0b60829d864fbc9b9e22144ff6494d066d4c3793
Harshith C S <harshithcs1004@gmail.com>
2025-02-24 22:08:23 +0530

    init commit

---
+++ src/main/java/com/travelplanner/models/User.java
@@ -0,0 +1,35 @@
+package com.travelplanner.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column
+    private String phoneNumber;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt = new Date();
+}
